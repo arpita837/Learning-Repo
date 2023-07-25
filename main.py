24ffe76202eb720435f879d87088ea31f1110051
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-25 11:31:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-def get_user_info():
-    name = "John Doe"
-    age = 30
-    email = "john.doe@example.com"
-    return name, age, email
+# Example of Update
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
 
-# Call the function and receive the multiple return values
-user_name, user_age, user_email = get_user_info()
+dict1.update(dict2)
+print(dict1)
 
-print(f"Name: {user_name}")
-print(f"Age: {user_age}")
-print(f"Email: {user_email}")
+# Example of Comparison
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'a': 1, 'b': 2}
+
+print(dict1 == dict2)
+
+dict3 = {'a': 1, 'b': 2, 'c': 3}
+
+print(dict1 != dict3)
+
+# Example of Length
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+print(len(my_dict))
+# Example of Copy
+original_dict = {'a': 1, 'b': 2}
+copied_dict = original_dict.copy()
+
+copied_dict['c'] = 3
+print(original_dict)
+print(copied_dict)
+
+# Example of Items
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+print(my_dict.items())
+
+# Example of String Representation
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+print(str(my_dict))
+
+
+
