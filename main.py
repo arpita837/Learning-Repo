8b7b8f1e15b08a5ca9e2b3d824dd33c66c1849e4
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-25 11:21:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def add(a , b):
-    return a+b
+def function_name(param1, param2=default_value2, param3=default_value3, ):
+    def greet(name, greeting="Hello"):
+        print(f"{greeting}, {name}!")
 
-print("The sum of 4 and 5 is:",add(4,5))
-print("The sum of 9 and 5 is:",add(9,5))
+    # Calling the function with only the required parameter
+    greet("Alice")  # Output: Hello, Alice!
+
+    # Calling the function with both required and optional parameters
+    greet("Bob", "Hi")  # Output: Hi, Bob!
+
+    # Calling the function without providing the optional parameter (uses default value)
+    greet("Charlie")  # Output: Hello, Charlie!
+
