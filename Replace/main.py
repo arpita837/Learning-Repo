cecc74c29e3e0fa3811b5fe131741623da10a064
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:01:43 +0530

    Create main.py

---
+++ Replace/main.py
@@ -0,0 +1,8 @@
+# Original string
+original_string = "Hello, World! Hello, Python!"
+
+# Replacing "Hello" with "Hi"
+new_string = original_string.replace("Hello", "Hi")
+
+print("Original string:", original_string)
+print("New string:", new_string)
