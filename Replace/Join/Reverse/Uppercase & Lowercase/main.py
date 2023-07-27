17504cab55861b712dd3d6b73e91dc2ede4fb120
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:08:03 +0530

    Create main.py

---
+++ Replace/Join/Reverse/Uppercase & Lowercase/main.py
@@ -0,0 +1,11 @@
+original_string = "Hello, World!"
+uppercase_string = original_string.upper()
+print(uppercase_string)
+
+original_string = "Hello, World!"
+lowercase_string = original_string.lower()
+print(lowercase_string)
+
+original_string = "Hello, World!"
+original_string = original_string.upper()  # Convert to uppercase and reassign
+print(original_string)
