e04bb5f04c675bdaba51aa272a602b2081ad079b
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:05:54 +0530

    Create main.py

---
+++ Replace/Join/Reverse/main.py
@@ -0,0 +1,20 @@
+# Reverse a string
+original_string = "Hello, World!"
+reversed_string = original_string[::-1]
+print(reversed_string)
+
+# Reverse a list
+original_list = [1, 2, 3, 4, 5]
+reversed_list = original_list[::-1]
+print(reversed_list)
+
+# Reverse a tuple
+original_tuple = (10, 20, 30, 40, 50)
+reversed_tuple = original_tuple[::-1]
+print(reversed_tuple)
+
+# Reverse a list in place
+original_list = [1, 2, 3, 4, 5]
+original_list.reverse()
+print(original_list)
+
