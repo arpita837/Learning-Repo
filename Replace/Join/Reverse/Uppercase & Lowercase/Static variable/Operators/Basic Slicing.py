2ab176c9f2d6619973bca92dac8e0885e3a5f40e
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:15:04 +0530

    Update Basic Slicing.py

--- Replace/Join/Reverse/Uppercase & Lowercase/Static variable/Operators/Basic Slicing.py
+++ Replace/Join/Reverse/Uppercase & Lowercase/Static variable/Operators/Basic Slicing.py
@@ -9,6 +9,19 @@ slice4 = original_list[4:]
 print(slice3)  # Output: [2, 3, 4]
 print(slice4)  # Output: [5, 6, 7, 8, 9]
 
+
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Extracting [2, 3, 4]
+slice3 = original_list[1:4]
+
+# Extracting [5, 6, 7, 8, 9]
+slice4 = original_list[4:]
+
+print(slice3)  # Output: [2, 3, 4]
+print(slice4)  # Output: [5, 6, 7, 8, 9]
+
+
 original_tuple = (10, 20, 30, 40, 50)
 # Extracting (20, 30, 40)
 slice5 = original_tuple[1:4]
