414a8d3abb256f81b28e102ce65c425ad232dca5
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:20:25 +0530

    Create  switch.py

---
+++ Replace/Join/Reverse/Uppercase & Lowercase/Static variable/Operators/Conditional Statement/switch.py
@@ -0,0 +1,15 @@
+def switch(option):
+    if option == 1:
+        print("You selected option 1.")
+    elif option == 2:
+        print("You selected option 2.")
+    elif option == 3:
+        print("You selected option 3.")
+    else:
+        print("Invalid option.")
+
+# Test the function with different options
+switch_example(1)  # Output: You selected option 1.
+switch_example(2)  # Output: You selected option 2.
+switch_example(3)  # Output: You selected option 3.
+switch_example(4)  # Output: Invalid option.
