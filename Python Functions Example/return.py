a2fbe5e3a25f65b6cc1baa35bf772be7a5e9a67c
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:34:20 +0530

    Create return.py

---
+++ Python Functions Example/return.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    product_result = a * b
+    return product_result
+
+# Function call with return value
+result = multiply(5, 3)
+print("Product:", result)  # Output: Product: 15
