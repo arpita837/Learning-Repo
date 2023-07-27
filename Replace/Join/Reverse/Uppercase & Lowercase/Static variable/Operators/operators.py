daa60016c2a31718621eb44f3625543dd30bc836
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:11:44 +0530

    Create operators.py

---
+++ Replace/Join/Reverse/Uppercase & Lowercase/Static variable/Operators/operators.py
@@ -0,0 +1,18 @@
+# Arithmetic operators
+a = 10
+b = 5
+c = a + b   # c = 15
+d = a * b   # d = 50
+e = a / b   # e = 2.0 (float division)
+
+# Comparison operators
+result1 = a > b   # True
+result2 = a == b  # False
+
+# Logical operators
+result3 = (a > b) and (a < 20)  # True (both conditions are True)
+
+# Membership operators
+my_list = [1, 2, 3, 4]
+result4 = 2 in my_list         # True
+result5 = 5 not in my_list     # True
