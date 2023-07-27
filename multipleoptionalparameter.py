904e36a1783c3441035049c88592324319cb8b08
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:37:48 +0530

    Create multipleoptionalparameter.py

---
+++ multipleoptionalparameter.py
@@ -0,0 +1,11 @@
+def send_email(subject, body, recipient_email=None, cc_email=None, bcc_email=None):
+    print(f"Sending email with subject: {subject}")
+    print(f"To: {recipient_email}")
+    if cc_email:
+        print(f"Cc: {cc_email}")
+    if bcc_email:
+        print(f"Bcc: {bcc_email}")
+
+# Calling the function with different combinations of parameters
+send_email("Greetings", "Hello there!", "alice@example.com")
+send_email("Meeting", "Agenda attached", "bob@example.com", "charlie@example.com")
