91bb13a314a386756a1725e7b73e89800a3da3df
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-27 18:27:14 +0530

    Create switch.py

---
+++ Looping Statements/switch.py
@@ -0,0 +1,9 @@
+def switch(option):
+    if option == 1:
+        print("You selected option 1.")
+    elif option == 2:
+        print("You selected option 2.")
+    elif option == 3:
+        print("You selected option 3.")
+    else:
+        print("Invalid option.")
