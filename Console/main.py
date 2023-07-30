95d1dc9b6b1a5cdd41699f286c299b5e747a1c61
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-30 11:47:48 +0530

    Create main.py

---
+++ Console/main.py
@@ -0,0 +1,9 @@
+# Taking a string input from the user
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+# Taking a numerical input from the user and performing calculations
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+print("The sum is:", sum_result)
