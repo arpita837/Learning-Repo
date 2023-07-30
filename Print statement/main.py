556a711ab82875332b6c6214340f4b73b84944c8
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-30 11:46:06 +0530

    Create main.py

---
+++ Print statement/main.py
@@ -0,0 +1,18 @@
+# Print a single value
+print("Hello, world!")
+# Output: Hello, world!
+
+# Print multiple values with a custom separator
+print("Python", "is", "awesome", sep="-")
+# Output: Python-is-awesome
+
+# Printing variables
+name = "Alice"
+age = 30
+print("Name:", name, "Age:", age)
+# Output: Name: Alice Age: 30
+
+# Changing the end character
+print("Hello", end=" ")
+print("World!")
+# Output: Hello World!
