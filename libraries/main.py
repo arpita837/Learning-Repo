235cb12ab34cdc8edea4405adc63478666927adb
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-30 11:02:23 +0530

    Create main.py

---
+++ libraries/main.py
@@ -0,0 +1,7 @@
+import math_operations
+
+result1 = math_operations.add(5, 3)
+result2 = math_operations.multiply(2, 4)
+
+print("Addition:", result1)      # Output: Addition: 8
+print("Multiplication:", result2)  # Output: Multiplication: 8
