f43e8d7c98fd15553e1aa48f650c7e9ca02273bd
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-30 11:19:39 +0530

    Create main.py

---
+++ Date Time/main.py
@@ -0,0 +1,6 @@
+import datetime
+current_datetime = datetime.datetime.now()
+# Format the datetime as a string
+formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_datetime)
+# Output: 2023-07-26 12:34:56
