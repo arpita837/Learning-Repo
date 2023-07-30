8dbcaa55c308939d3cdd5d5d6172e233c100c94e
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-30 11:17:06 +0530

    Create specificdatetime.py

---
+++ Date Time/specificdatetime.py
@@ -0,0 +1,5 @@
+import datetime
+# Create a specific date and time: year, month, day, hour, minute, second, microsecond
+specific_datetime = datetime.datetime(2023, 7, 26, 12, 30, 0, 0)
+print(specific_datetime)
+# Output: 2023-07-26 12:30:00
