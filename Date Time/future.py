fb17cd33a0e03d9cd179c239677d47af8c48e002
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-07-30 11:20:35 +0530

    Create future.py

---
+++ Date Time/future.py
@@ -0,0 +1,8 @@
+import datetime
+current_datetime = datetime.datetime.now()
+future_datetime = current_datetime + datetime.timedelta(days=7)
+print("Current Datetime:", current_datetime)
+print("Future Datetime:", future_datetime)
+# Output:
+# Current Datetime: 2023-07-26 12:34:56.789012
+# Future Datetime: 2023-08-02 12:34:56.789012
